8a2e53bed731bde24d6b25791e6f0c152702e100
Kernael92 <kernaelapuko5@gmail.com>
2018-12-06 12:07:18 +0300

    get the day of the week the project was posted

---
+++ projects/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt 
+
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Kernel Portfolio')
+def projects_of_day(request):
+    date = dt.date.today()
+
+    # Function to convert date object to find exact day
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Projects for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+                '''
+    return HttpResponse(html)
+def convert_dates(dates):
+    # function that gets the weekday number of the date
+    day_number = dt.date.weekday(dates)
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+

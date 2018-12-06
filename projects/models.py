d149247f4e4639ae89f9c175b1c417aa9439f128
Kernael92 <kernaelapuko5@gmail.com>
2018-12-06 23:01:02 +0300

    create unit test for each model class

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Developer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    userName = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.userName
+
+class Repository(models.Model):
+    title = models.CharField(max_length =30)
+    description = models.CharField(max_length = 245)
+    developer = models.ForeignKey(Developer)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Developer,Repository
+
 
 # Create your tests here.
+# Set up method
+class DeveloperTestClass(TestCase):
+    def setUp(self):
+        self.kernaeljoy = Developer(first_name = "Kernael", last_name = "Joy", userName = "kernaeljoy", email = "kerry@gmail.com")
+    # testing for the instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kernaeljoy,Developer))
+
+
+

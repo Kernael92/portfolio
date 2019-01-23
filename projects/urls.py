7c6d315a6a442d81f37f9d6b87b18be7afd0bf98
Kernael92 <kernaelapuko5@gmail.com>
2019-01-23 14:05:52 +0300

    style the landing page

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.projects_today,name='projectsToday'),
-    url(r'^archives/(\d{4})-\d{2}-\d{2}/$',views.past_days_projects,name='pastProjects')
+  url(r'^$',views.index, name='index'),
+  url(r'^self/',views.self, name='self'),
+    
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -4,22 +4,9 @@ import datetime as dt
 
 
 # Create your views here.
-def projects_today(request):
-    date = dt.date.today()
-    return render(request,'all-projects/today-projects.html', {"date": date,})
+def index(request):
+    return render(request,'index.html')
 
-def past_days_projects(request,past_date):
-    try:
-        # Converts data from the string url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-    if date == dt.date.today():
-        return redirect(projects_today)
-    return render(request, 'all-projects/past-projects.html', {"date":date})
-
-
-
+def self(request):
+    return render(request,'portfolio/self.html')
 
